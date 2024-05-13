41dc2e9931a05e213b86e534d4a8c6673b000320
Jessica GUALTIERI <jessica.gualtieri57050@gmail.com>
2024-05-13 09:24:41 +0200

    First Flutter project : calculator

---
+++ calculator/lib/calculator.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+class Calculator extends StatefulWidget {
+  const Calculator({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<Calculator> createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  int _counter = 0;
+  final int _increment = 2;
+  int _result = 2;
+  int _clickCount = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter += 2;
+      _result = _counter + _increment;
+      _clickCount++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (_counter > 0)
+              Row (
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                    Text(
+                    'Vous avez cliqué $_clickCount fois',
+                  ),
+                ],
+              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  '$_counter + $_increment =',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+                Text(
+                  '$_result',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '+2',
+              style: Theme.of(context).textTheme.headlineSmall,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
